09cbe516483d7c93d017db6e3da4c2c3a1765546
farizkhanzada <128466693+farizkhanzada@users.noreply.github.com>
2024-07-16 14:04:57 +0500

    Solved the "Shipment Number Required" Issue

--- manifest_data/manifest_data/doctype/manifest_upload_data/manifest_upload_data.py
+++ manifest_data/manifest_data/doctype/manifest_upload_data/manifest_upload_data.py
@@ -6,6 +6,8 @@ from frappe.utils.background_jobs import enqueue
 # Define the insert_data function outside of the class
 
 def insert_data(arrays, frm, to):
+    shipment_num = None
+    
     for line in arrays:
         # Extract the doctype name using slicing (assuming the name is at index 45-50)
         doctype_name = "R" + line[frm:to].strip()
@@ -18,48 +20,45 @@ def insert_data(arrays, frm, to):
                 if row.field_name == "shipment_number":
                     shipst=row.from_index-1
                     shipto=row.to_index
+                    shipment_num = line[shipst:shipto].strip()
                     break
         except frappe.DoesNotExistError:
             # If doctype_name doesn't exist in Definitions, move to the next line
             continue
-        shipment_num = line[shipst:shipto].strip()
-        # frappe.msgprint(str(shipst)   +" "+ str(shipto))
-            # Create a new document for the current doctype
 
-        ex = frappe.db.exists(doctype_name , shipment_num)
-        # frappe.msgprint(str(doctype_name)+" "+str(shipment_num))
-        if ex:
-            doc = frappe.get_doc(doctype_name, shipment_num)
-            
+        docs = frappe.get_list(doctype_name, filters={'shipment_number': shipment_num})
+        if docs :
+            docss = frappe.get_doc(doctype_name , docs[0])
             for child_record in definition.definitions:
                 field_name = child_record.field_name
                 from_index = child_record.from_index - 1
                 to_index = child_record.to_index
                 # Search and extract field_data from line using from_index and to_index
                 field_data = line[from_index:to_index].strip()
-                doc.set(field_name, field_data)
-            doc.save()
+                docss.set(field_name, field_data)
+            docss.save()
             frappe.db.commit()
+            print(doctype_name, shipment_num, "Updating")
                 
         
         else:
-            doc = frappe.new_doc(doctype_name)
             
-            # Iterate over each child record
-            for child_record in definition.definitions:
-                field_name = child_record.field_name
-                from_index = child_record.from_index -1
-                to_index = child_record.to_index
-
-                # Search and extract field_data from line using from_index and to_index
-                field_data = line[from_index:to_index].strip()
-                # Set the field data in the document
-                doc.set(field_name, field_data)
-
-            # Save the new document
-            
-            doc.insert()
-            doc.save()
+                doc = frappe.new_doc(doctype_name)
+                
+                # Iterate over each child record
+                for child_record in definition.definitions:
+                    field_name = child_record.field_name
+                    from_index = child_record.from_index -1
+                    to_index = child_record.to_index
+                    # Search and extract field_data from line using from_index and to_index
+                    field_data = line[from_index:to_index].strip()
+                    # Set the field data in the document
+                    doc.set(field_name, field_data)
+                    
+                # Save the new document
+                print(doctype_name , shipment_num,"Inserting")
+                doc.insert()
+                doc.save()
             
 
 class ManifestUploadData(Document):
@@ -75,7 +74,7 @@ class ManifestUploadData(Document):
             arrays = content.split('\n')
             frm = int(self.from_index)-1
             to = int(self.to_index)
-            chunk_size = 50  # Number of lines per chunk
+            chunk_size = 10  # Number of lines per chunk
             current_index = 0  # Starting index for slicing the array
 
             # Continue processing chunks until all lines are processed
@@ -86,4 +85,3 @@ class ManifestUploadData(Document):
                 current_index += chunk_size
                 enqueue(insert_data, arrays=chunk,frm=frm, to=to, queue="default")
             # insert_data( arrays,frm, to)
-            
